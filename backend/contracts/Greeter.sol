d6ad86e52e58e7c63e1fefb2098c89e477bf2070
juliensie <julien.sie@gmail.com>
2021-11-22 16:47:50 +0100

    set greeting

--- backend/contracts/Greeter.sol
+++ backend/contracts/Greeter.sol
@@ -2,4 +2,13 @@
 pragma solidity >=0.4.16 <0.9.0;
 
 contract Greeter {
+    string private _greeting = "Hello, World!";
+
+    function greet() external view returns (string memory) {
+        return _greeting;
+    }
+
+    function setGreeting(string calldata greeting) external {
+        _greeting = greeting;
+    }
 }
--- backend/test/greeter_test.js
+++ backend/test/greeter_test.js
@@ -1,8 +1,29 @@
 const GreeterContract = artifacts.require("Greeter");
 
 contract("Greeter", () => {
-    it("has been deployed successfully",async () => {
-        const c = await GreeterContract.deployed();
-        assert(c, "contract was not deployed :( ");
+    it("has been deployed successfully", async () => {
+        const ctr = await GreeterContract.deployed();
+        assert(ctr, "contract was not deployed :( ");
+    });
+
+    describe("greet()", () => {
+        const expected = "Hello, World!";
+        it(`returns '${expected}'`, async () => {
+            const greeter = await GreeterContract.deployed();
+            const greet = await greeter.greet();
+            assert.equal(greet, expected, `greet should be '${expected}'`);
+        });
+    });
+});
+
+contract("Greeter: update greeting", () => {
+    describe("setGreeting(string)", () => {
+        it("sets greeting to passed in string", async () => {
+            const greeter = await GreeterContract.deployed();
+            const expected = "Hi there!";
+            await greeter.setGreeting(expected);
+            const greet = await greeter.greet();
+            assert.equal(greet, expected, `greeting not updated with string '${expected}'`);
+        });
     });
 });
