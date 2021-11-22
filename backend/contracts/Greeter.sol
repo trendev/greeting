18ea77778978c0b8288c92a35a511f73d3513482
juliensie <julien.sie@gmail.com>
2021-11-22 23:03:19 +0100

    openzeppelin + test fallback() revert

--- backend/contracts/Greeter.sol
+++ backend/contracts/Greeter.sol
@@ -1,7 +1,10 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.4.16 <0.9.0;
 
-contract Greeter {
+// Import Ownable from the OpenZeppelin Contracts library
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract Greeter is Ownable {
     string private _greeting = "Hello, World!";
     address private _owner;
 
@@ -10,19 +13,23 @@ contract Greeter {
     //     _;
     // }
 
-    constructor() {
-        _owner = msg.sender;
-    }
+    // constructor() {
+    //     _owner = msg.sender;
+    // }
 
     function greet() external view returns (string memory) {
         return _greeting;
     }
 
-    function setGreeting(string calldata greeting) external onlyOwner{
+    function setGreeting(string calldata greeting) external onlyOwner {
         _greeting = greeting;
     }
 
-    function owner() public view returns (address) {
-        return _owner;
+    // function owner() public view returns (address) {
+    //     return _owner;
+    // }
+
+    fallback() external {
+        // dont receive ether via fallback method (by not having 'payable' modifier on this function).
     }
 }
--- backend/test/greeter_test.js
+++ backend/test/greeter_test.js
@@ -2,23 +2,37 @@ const GreeterContract = artifacts.require("Greeter");
 
 contract("Greeter", (accounts) => {
     it("has been deployed successfully", async () => {
-        const ctr = await GreeterContract.deployed();
-        assert(ctr, "contract was not deployed :( ");
+        const instance = await GreeterContract.deployed();
+        assert(instance, "contract was not deployed :( ");
     });
 
     describe("greet()", () => {
         const expected = "Hello, World!";
         it(`returns '${expected}'`, async () => {
-            const greeter = await GreeterContract.deployed();
-            const grt = await greeter.greet();
+            const instance = await GreeterContract.deployed();
+            const grt = await instance.greet();
             assert.equal(grt, expected);
+        });
+    });
+
+    //instance.send(web3.utils.toWei(1, "ether"))
+    describe("fallback()", () => {
+        it("should revert", async () => {
+            const instance = await GreeterContract.deployed();
+            try {
+                await instance.send(web3.utils.toWei("1", "ether"));
+            } catch (err) {
+                assert(err.message.endsWith("revert"),"error message should end with 'revert'");
+                return;
+            }
+            assert(false, "should never occur");
         });
     });
 
     describe("owner()", () => {
         it("returns the owner's address", async () => {
-            const greeter = await GreeterContract.deployed();
-            const owner = await greeter.owner();
+            const instance = await GreeterContract.deployed();
+            const owner = await instance.owner();
             const a0 = accounts[0];
 
             assert(owner, "owner exists");
@@ -30,24 +44,24 @@ contract("Greeter", (accounts) => {
 contract("Greeter: update greeting", (accounts) => {
     describe("setGreeting(string)", () => {
         it("owner sets greeting to passed in string", async () => {
-            const greeter = await GreeterContract.deployed();
+            const instance = await GreeterContract.deployed();
             const expected = "Hi there!";
-            await greeter.setGreeting(expected);
-            const grt = await greeter.greet();
+            await instance.setGreeting(expected);
+            const grt = await instance.greet();
             assert.equal(grt, expected);
         });
 
         it("unauthorized address cannot set greeting to passed in string", async () => {
-            const greeter = await GreeterContract.deployed();
+            const instance = await GreeterContract.deployed();
             const grt = "Another greeting";
-            const g1 = await greeter.greet();
+            const g1 = await instance.greet();
             try {
-                await greeter.setGreeting(grt, { from: accounts[1] });
+                await instance.setGreeting(grt, { from: accounts[1] });
             } catch (err) {
                 const errmsg = "Ownable: caller is not the owner";
                 assert.equal(err.reason, errmsg);
             }
-            const g2 = await greeter.greet();
+            const g2 = await instance.greet();
             assert.equal(g1, g2);
             assert.notEqual(g2, grt);
         });
