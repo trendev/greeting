83d9e589974adc43121546efd2f05161d639e3ab
juliensie <julien.sie@gmail.com>
2021-11-25 13:40:06 +0100

    refactor tests

--- backend/test/greeter_test.js
+++ backend/test/greeter_test.js
@@ -1,28 +1,41 @@
 const GreeterContract = artifacts.require("Greeter");
 
+let deployed = [];
+
 contract("Greeter", (accounts) => {
+    let instance;
+    beforeEach(async () => {
+        instance = await GreeterContract.deployed(); // provides the same deployed contract
+        // instance = await GreeterContract.new(); // provides new contracts
+        deployed.push(Promise.resolve(instance.address));
+    });
+
+    after(() => {
+        Promise.all(deployed).then(dc => {
+            r = dc.filter((e, i) => dc.indexOf(e) == i); // 
+            console.log(`** [${r && r.length}] GreeterContract deployed / called ${dc.length} times **`);
+            r.forEach(c => console.log(`address : ${c}`));
+        });
+    });
+
     it("has been deployed successfully", async () => {
-        const instance = await GreeterContract.deployed();
         assert(instance, "contract was not deployed :( ");
     });
 
     describe("greet()", () => {
         const expected = "Hello, World!";
         it(`returns '${expected}'`, async () => {
-            const instance = await GreeterContract.deployed();
             const grt = await instance.greet();
             assert.equal(grt, expected);
         });
     });
 
-    //instance.send(web3.utils.toWei(1, "ether"))
     describe("fallback()", () => {
         it("should revert", async () => {
-            const instance = await GreeterContract.deployed();
             try {
                 await instance.send(web3.utils.toWei("1", "ether"));
             } catch (err) {
-                assert(err.message,"fallback() should throw an error message");
+                assert(err.message, "fallback() should throw an error message");
                 return;
             }
             assert(false, "should never occur");
@@ -31,7 +44,6 @@ contract("Greeter", (accounts) => {
 
     describe("owner()", () => {
         it("returns the owner's address", async () => {
-            const instance = await GreeterContract.deployed();
             const owner = await instance.owner();
             const a0 = accounts[0];
 
@@ -39,26 +51,22 @@ contract("Greeter", (accounts) => {
             assert.equal(owner, a0);
         });
     });
-});
 
-contract("Greeter: update greeting", (accounts) => {
     describe("setGreeting(string)", () => {
-        it("owner sets greeting to passed in string", async () => {
-            const instance = await GreeterContract.deployed();
+        it("owner", async () => {
             const expected = "Hi there!";
             await instance.setGreeting(expected);
             const grt = await instance.greet();
             assert.equal(grt, expected);
         });
 
-        it("unauthorized address cannot set greeting to passed in string", async () => {
-            const instance = await GreeterContract.deployed();
+        it("restricted", async () => {
             const grt = "Another greeting";
             const g1 = await instance.greet();
             try {
                 await instance.setGreeting(grt, { from: accounts[1] });
             } catch (err) {
-                assert(err.message,"setGreeting() should throw an error message");
+                assert(err.message, "setGreeting() should throw an error message");
             }
             const g2 = await instance.greet();
             assert.equal(g1, g2);
@@ -66,3 +74,4 @@ contract("Greeter: update greeting", (accounts) => {
         });
     });
 });
+
