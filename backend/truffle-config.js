4f48998c991f99444ead4be1ad545b6559d999fb
juliensie <julien.sie@gmail.com>
2021-11-28 22:50:37 +0100

    add ropsten network using alchemy

--- backend/truffle-config.js
+++ backend/truffle-config.js
@@ -68,6 +68,17 @@ module.exports = {
       network_id: "*"
     },
 
+    ropsten: {
+      provider: () => {
+        const project_id = process.env["ALCHEMY_PROJECT_ID"]
+        
+        return new HDWalletProvider(
+          mnemonic,
+          `https://eth-ropsten.alchemyapi.io/v2/${project_id}`);
+      },
+      network_id: "*"
+    },
+
     // avalanche testnet
     fuji: {
       provider: () => {        
