d585f3a3c7123d0443da9c3e58e94606992beb04
juliensie <julien.sie@gmail.com>
2021-12-17 19:05:54 +0100

    add ethers.js

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ethers } from 'ethers';
+import { from } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.sass']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   greeting = 'hello world 😘';
+  blockn = 0;
+  network: any;
+
+  ngOnInit() {
+    const provider = new ethers.providers.Web3Provider((window as any).ethereum);
+    // const signer = provider.getSigner();
+    if (provider) {
+      from(provider.getBlockNumber()).subscribe(v => this.blockn = v);
+      from(provider.getNetwork()).subscribe(v => this.network = JSON.stringify(v, null, 2));
+    }
+
+  }
+
 }
