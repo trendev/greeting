5dca063c9eb762e6e217bdd051cd4a1481e239d6
juliensie <julien.sie@gmail.com>
2022-01-25 14:54:55 +0100

    refactor with Promise chains

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -11,33 +11,34 @@ export class AppComponent implements OnInit {
 
   blockNumber: Promise<number>;
   network: Promise<providers.Network>;
-  balance: string;
+  balance: Promise<string>;
   address: Promise<string>;
-  private _isConnected: boolean;
+  private _isConnected = false;
 
   constructor(private ethService: EthService) { }
 
   ngOnInit() {
-    this.initProviderAndFetchData();
-  }
-
-  private async initProviderAndFetchData() {
-    await this.ethService.init();
-    this.fetchData();
+    this.ethService.init()
+      .then(() => this.fetchData())
+      .then(connected => this._isConnected = connected);
   }
 
   get isConnected(): boolean {
     return this.ethService.isConnected() && this._isConnected;
   }
-  
-  private async fetchData() {
-    this.blockNumber = this.ethService.getBlockNumber();
-    this.network = this.ethService.getNetwork();
 
-    this.address = this.ethService.getSigner().getAddress();
-    const balance = await this.ethService.getSigner().getBalance('latest');
-    this.balance = utils.formatUnits(balance, 18); //@TODO : create a Directive
-    this._isConnected = true;
+  private async fetchData() {
+    try {
+      this.blockNumber = this.ethService.getBlockNumber();
+      this.network = this.ethService.getNetwork();
+      this.address = this.ethService.getSigner().getAddress();
+      this.balance = this.ethService.getSigner().getBalance('latest')
+        .then(balance => utils.formatUnits(balance, 18)); //@TODO : create a Directive
+    } catch (err) {
+      console.error(err);
+      return false;
+    }
+    return true;
   }
 
 }
