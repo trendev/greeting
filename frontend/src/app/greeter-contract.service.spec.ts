0f7c257923d84a258ad19f8fa4869dd4af291f9d
juliensie <julien.sie@gmail.com>
2022-01-26 11:55:20 +0100

    testing smart contract services

--- frontend/src/app/greeter-contract.service.spec.ts
+++ frontend/src/app/greeter-contract.service.spec.ts
@@ -1,3 +1,4 @@
+import { catchError, finalize, of } from 'rxjs';
 import { TestBed } from '@angular/core/testing';
 
 import { GreeterContractService } from './greeter-contract.service';
@@ -12,5 +13,20 @@ describe('GreeterContractService', () => {
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should not be deployed', () => {
+    expect(service.isDeployed()).toBeFalsy();
+  });
+
+  it('should throw a TypeError if not deployed', () => {
+    try {
+      service.logs().subscribe();
+    } catch (err) {
+      expect(err).toBeInstanceOf(TypeError);
+      if (err instanceof TypeError) {
+        expect(err.message).toContain(`Cannot read properties of undefined`);
+      }
+    }
   });
 });
