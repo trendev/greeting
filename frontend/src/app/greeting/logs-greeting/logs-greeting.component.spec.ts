6a7fc31ff639d7955df46b57aeb445652bf20655
juliensie <julien.sie@gmail.com>
2022-02-08 13:19:44 +0100

    format logs

--- frontend/src/app/greeting/logs-greeting/logs-greeting.component.spec.ts
+++ frontend/src/app/greeting/logs-greeting/logs-greeting.component.spec.ts
@@ -1,10 +1,11 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, fakeAsync } from '@angular/core/testing';
 
 import { LogsGreetingComponent } from './logs-greeting.component';
 
 describe('LogsGreetingComponent', () => {
   let component: LogsGreetingComponent;
   let fixture: ComponentFixture<LogsGreetingComponent>;
+  const n = 10;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -27,4 +28,24 @@ describe('LogsGreetingComponent', () => {
     expect(component.logs).toBeDefined();
     expect(component.logs).toHaveSize(0);
   });
+  
+  it(`should have ${n} logs`, fakeAsync(() => {
+    expect(component.logs).toBeDefined();
+
+    for (let i = 0; i < n; i++) {
+      component.logs.push(`log-${i}`);
+    }
+
+    expect(component.logs).toHaveSize(n);
+
+    fixture.detectChanges();
+
+    const appElement: HTMLElement = fixture.nativeElement;
+    expect(appElement).toBeDefined();
+    const elmt = appElement.querySelector('table')!;
+    expect(elmt).toBeDefined();
+    expect(elmt.hasChildNodes()).toBeTrue();
+    expect(elmt.childElementCount).toEqual(n + 1); // header + n rows (logs)
+
+  }));
 });
