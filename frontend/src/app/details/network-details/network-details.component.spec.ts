9a1dfcbe4ae58eaccd22965c6afbf13fd0e4c81c
juliensie <julien.sie@gmail.com>
2022-02-12 15:05:06 +0100

    stub dialogRef usage

--- frontend/src/app/details/network-details/network-details.component.spec.ts
+++ frontend/src/app/details/network-details/network-details.component.spec.ts
@@ -1,9 +1,14 @@
+import { of } from 'rxjs';
 import { MatCardModule } from '@angular/material/card';
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { providers } from 'ethers';
 import { EthService } from 'src/app';
 
 import { NetworkDetailsComponent } from './network-details.component';
+import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
+import { ComponentType } from '@angular/cdk/portal';
+import { DialogSelectEthNetworkComponent } from './dialog-select-eth-network/dialog-select-eth-network.component';
+
 
 describe('NetworkDetailsComponent', () => {
   let component: NetworkDetailsComponent;
@@ -13,9 +18,10 @@ describe('NetworkDetailsComponent', () => {
   const network: providers.Network = { name: "net", chainId: 7777, ensAddress: '0xab01234cd56789ef' };
 
   beforeEach(async () => {
-    ethServiceSpy = jasmine.createSpyObj<EthService>('EthService', ['getNetwork', 'addEthNetwork']);
+    ethServiceSpy = jasmine.createSpyObj<EthService>('EthService', ['getNetwork', 'addEthNetwork', 'getCustomNetworksNames']);
     ethServiceSpy.getNetwork.and.resolveTo(network);
     ethServiceSpy.addEthNetwork.and.resolveTo(true);
+    ethServiceSpy.getCustomNetworksNames.and.returnValue([network.name]);
 
     await TestBed.configureTestingModule({
       imports: [
@@ -25,6 +31,15 @@ describe('NetworkDetailsComponent', () => {
       providers: [{
         provide: EthService,
         useValue: ethServiceSpy
+      }, {
+        provide: MatDialog,
+        useValue: {
+          open: (component: ComponentType<DialogSelectEthNetworkComponent>, config: MatDialogConfig | undefined) => {
+            return {
+              afterClosed: () => of(network.name)
+            }
+          }
+        }
       }]
     }).compileComponents();
 
@@ -68,7 +83,7 @@ describe('NetworkDetailsComponent', () => {
   }));
 
   it('should add/switch ethereum network', waitForAsync(() => {
-    const spy = spyOn(console,'error');
+    const spy = spyOn(console, 'error');
     fixture.whenStable().then(() => {
       fixture.detectChanges();
       component.addEthNetwork();
