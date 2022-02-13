1ba93bb36a4c9f8caf6203df2ac472b6867345b0
juliensie <julien.sie@gmail.com>
2022-02-13 21:57:13 +0100

    deploy on bsc

--- backend/truffle-config.js
+++ backend/truffle-config.js
@@ -111,7 +111,7 @@ module.exports = {
     bnbt: {
       provider: () => new HDWalletProvider(
         mnemonic,
-        `wss://data-seed-prebsc-1-s1.binance.org:8545`),
+        `https://data-seed-prebsc-2-s2.binance.org:8545`),
       network_id: 97,
       confirmations: 10,
       timeoutBlocks: 200,
