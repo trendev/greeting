0d179218afbda5d3c978ee843bc0fa3d0fdf70c2
_whyvrafvr <jsie@trendev.fr>
2022-02-27 00:54:00 +0100

    display old greeting with golang

--- backend/go-greeting/main.go
+++ backend/go-greeting/main.go
@@ -1,11 +1,16 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	greeter "go-greeting/contracts"
 	"log"
+	"strings"
 
+	"github.com/ethereum/go-ethereum"
+	"github.com/ethereum/go-ethereum/accounts/abi"
 	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/crypto"
 	"github.com/ethereum/go-ethereum/ethclient"
 )
 
@@ -29,4 +34,41 @@ func main() {
 		log.Fatal(err)
 	}
 	fmt.Printf("📩 Current greeting: %s\n", greet)
+
+	query := ethereum.FilterQuery{
+		Addresses: []common.Address{
+			address,
+		},
+	}
+
+	logs, err := client.FilterLogs(context.Background(), query)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	l := len(logs)
+	fmt.Printf("🗃 Previous greets : %d\n", l)
+
+	contractAbi, err := abi.JSON(strings.NewReader(string(greeter.GreeterABI)))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	logGreetingUpdatedSig := []byte("GreetingUpdated(address,string,string)")
+	logGreetingUpdatedSigHash := crypto.Keccak256Hash(logGreetingUpdatedSig)
+
+	for _, lg := range logs {
+		switch lg.Topics[0].Hex() {
+		case logGreetingUpdatedSigHash.Hex():
+			var event greeter.GreeterGreetingUpdated
+
+			err := contractAbi.UnpackIntoInterface(&event, "GreetingUpdated", lg.Data)
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			fmt.Printf(" 💬 %s\n", event.OldGreeting)
+		}
+	}
+
 }
