1ecb1a77713e49152b15ac6f9c82a65c7a8bc150
_whyvrafvr <jsie@trendev.fr>
2022-07-24 15:07:18 +0200

    use map for networks / contract address

--- backend/go-greeting/main.go
+++ backend/go-greeting/main.go
@@ -12,18 +12,27 @@ import (
 	"github.com/ethereum/go-ethereum/ethclient"
 )
 
+var networks = map[string]struct {
+	url     string
+	address string
+}{
+	"ropsten": {"https://ropsten.infura.io/v3", "0xD1441e1Cb432568B6a1D42aEBd9Ce73b2CE4f475"},
+	"fuji":    {"https://avalanche-fuji.infura.io/v3", "0x97365BecdD17ed5A9E2489c121775AFC88E21260"},
+}
+
 func main() {
 	id := os.Getenv("INFURA_PROJECT_ID")
 	if id == "" {
 		log.Fatalln("INFURA_PROJECT_ID must be set")
 	}
 
-	client, err := ethclient.Dial(fmt.Sprintf("https://ropsten.infura.io/v3/%s", id))
+	net := "ropsten"
+	client, err := ethclient.Dial(fmt.Sprintf("%s/%s", networks[net].url, id))
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	a := "0xD1441e1Cb432568B6a1D42aEBd9Ce73b2CE4f475"
+	a := networks[net].address
 	address := common.HexToAddress(a)
 
 	instance, err := greeter.NewGreeter(address, client)
